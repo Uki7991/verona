28891b842df819f3bd6410db868e9d5d3014498b
Uki7991 <tilek.kubanov@gmail.com>
2018-04-01 20:40:51 +0600

    all

--- app/Http/Controllers/FeatureController.php
+++ app/Http/Controllers/FeatureController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Feature;
 use Illuminate\Http\Request;
 
 class FeatureController extends Controller
 {
-    //
+    public function index() {
+        return view('', [
+            'features' => Feature::all(),
+        ]);
+    }
 }
