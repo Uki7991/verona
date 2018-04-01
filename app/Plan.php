88303e9c3bc1cbece2a1844f5f083b79ff5115ea
Uki7991 <tilek.kubanov@gmail.com>
2018-04-01 21:56:04 +0600

    Added relationships

--- app/Plan.php
+++ app/Plan.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Plan extends Model
 {
     protected $guarded = [];
+
+    public function object() {
+        return $this->belongsTo('App\Object');
+    }
 }
