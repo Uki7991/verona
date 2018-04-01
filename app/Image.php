ab52f491e3545a854d89fca7c8cf081b22bba9b4
Uki7991 <tilek.kubanov@gmail.com>
2018-04-01 19:09:25 +0600

    Added model, migrations and controllers

--- app/Image.php
+++ app/Image.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Verona extends Model
+class Image extends Model
 {
     //
 }
--- app/Object.php
+++ app/Object.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Advantages extends Model
+class Object extends Model
 {
-    //
+
 }
