c63ac97eb5ad372f9c01374ad7c9c9a90c434c45
Uki7991 <tilek.kubanov@gmail.com>
2018-05-18 02:16:06 +0600

    view changed

--- resources/views/backend/opisanie.blade.php
+++ resources/views/backend/opisanie.blade.php
@@ -18,158 +18,183 @@
             @include('layouts.navigator')
         </div>
 
-        <div class="row mb-5 justify-content-center">
-            <div class="col-10" style=" padding:12px; background-image:url(images/stipes2.png); background-size: 100px 100px;" >
-                <div class="item">
-                    <img src="/images/Renders/01_Day.jpg" alt="" class="img-fluid">
-                    <form action="" enctype="multipart/form-data">
-                        {{ csrf_field() }}
-                        <input type="file">
-                        <button type="submit" class="btn btn-warning">Редактировать</button>
-                    </form>
-                    <a href="" class="btn btn-danger">Удалить</a>
+        <div class="row mb-5 bg-secondary">
+            <div class="col-4 my-3">
+                <div class="card">
+                    <img class="card-img-top" src="/images/Renders/01_Day.jpg" alt="Card image cap">
+                    <div class="card-body justify-content-center text-center">
+                        <form action="" enctype="multipart/form-data">
+                            {{ csrf_field() }}
+                            <input type="file">
+                            <button type="submit" class="btn btn-warning mt-2">Редактировать</button>
+                        </form>
+                    </div>
+                    <div class="card-body text-center">
+                        <a href="" class="btn btn-danger">Удалить</a>
+                    </div>
                 </div>
-                <div class="item">
-                    <img src="/images/Renders/01_Evening.jpg" alt="" class="img-fluid">
-                    <form action="" enctype="multipart/form-data">
-                        {{ csrf_field() }}
-                        <input type="file">
-                        <button type="submit" class="btn btn-warning">Редактировать</button>
-                    </form>
-                    <a href="" class="btn btn-danger">Удалить</a>
+            </div>
+
+
+            <div class="col-4 my-3">
+                <div class="card">
+                    <img class="card-img-top" src="/images/Renders/01_Day.jpg" alt="Card image cap">
+                    <div class="card-body justify-content-center text-center">
+                        <form action="" enctype="multipart/form-data">
+                            {{ csrf_field() }}
+                            <input type="file">
+                            <button type="submit" class="btn btn-warning mt-2">Редактировать</button>
+                        </form>
+                    </div>
+                    <div class="card-body text-center">
+                        <a href="" class="btn btn-danger">Удалить</a>
+                    </div>
                 </div>
-                <div class="item">
-                    <img src="/images/Renders/02_Day.jpg" alt="" class="img-fluid">
-                    <form action="" enctype="multipart/form-data">
-                        {{ csrf_field() }}
-                        <input type="file">
-                        <button type="submit" class="btn btn-warning">Редактировать</button>
-                    </form>
-                    <a href="" class="btn btn-danger">Удалить</a>
+            </div>
+
+
+            <div class="col-4 my-3">
+                <div class="card">
+                    <img class="card-img-top" src="/images/Renders/01_Day.jpg" alt="Card image cap">
+                    <div class="card-body justify-content-center text-center">
+                        <form action="" enctype="multipart/form-data">
+                            {{ csrf_field() }}
+                            <input type="file">
+                            <button type="submit" class="btn btn-warning mt-2">Редактировать</button>
+                        </form>
+                    </div>
+                    <div class="card-body text-center">
+                        <a href="" class="btn btn-danger">Удалить</a>
+                    </div>
                 </div>
-                <div class="item">
-                    <img src="/images/Renders/02_Evening.jpg" alt="" class="img-fluid">
-                    <form action="" enctype="multipart/form-data">
-                        {{ csrf_field() }}
-                        <input type="file">
-                        <button type="submit" class="btn btn-warning">Редактировать</button>
-                    </form>
-                    <a href="" class="btn btn-danger">Удалить</a>
+            </div>
+
+
+            <div class="col-4 my-3">
+                <div class="card">
+                    <img class="card-img-top" src="/images/Renders/01_Day.jpg" alt="Card image cap">
+                    <div class="card-body justify-content-center text-center">
+                        <form action="" enctype="multipart/form-data">
+                            {{ csrf_field() }}
+                            <input type="file">
+                            <button type="submit" class="btn btn-warning mt-2">Редактировать</button>
+                        </form>
+                    </div>
+                    <div class="card-body text-center">
+                        <a href="" class="btn btn-danger">Удалить</a>
+                    </div>
                 </div>
-                <div class="item">
-                    <img src="/images/Renders/03_Day.jpg" alt="" class="img-fluid">
-                    <form action="" enctype="multipart/form-data">
-                        {{ csrf_field() }}
-                        <input type="file">
-                        <button type="submit" class="btn btn-warning">Редактировать</button>
-                    </form>
-                    <a href="" class="btn btn-danger">Удалить</a>
+            </div>
+
+
+            <div class="col-4 my-3">
+                <div class="card">
+                    <img class="card-img-top" src="/images/Renders/01_Day.jpg" alt="Card image cap">
+                    <div class="card-body justify-content-center text-center">
+                        <form action="" enctype="multipart/form-data">
+                            {{ csrf_field() }}
+                            <input type="file">
+                            <button type="submit" class="btn btn-warning mt-2">Редактировать</button>
+                        </form>
+                    </div>
+                    <div class="card-body text-center">
+                        <a href="" class="btn btn-danger">Удалить</a>
+                    </div>
                 </div>
-                <div class="item">
-                    <img src="/images/Renders/03_Evening.jpg" alt="" class="img-fluid">
-                    <form action="" enctype="multipart/form-data">
-                        {{ csrf_field() }}
-                        <input type="file">
-                        <button type="submit" class="btn btn-warning">Редактировать</button>
-                    </form>
-                    <a href="" class="btn btn-danger">Удалить</a>
+            </div>
+
+            <div class="col-4 my-3">
+                <div class="card">
+                    <div class="card-body justify-content-center text-center">
+                        <h5 class="card-title text-success">Новая картинка</h5>
+                        <form action="" class="mt-5" enctype="multipart/form-data">
+                            {{ csrf_field() }}
+                            <input type="file">
+                            <button type="submit" class="btn btn-success mt-3">Еще картинка</button>
+                        </form>
+                    </div>
                 </div>
-                <div class="item">
-                    <img src="/images/Renders/06_Day.jpg" alt="" class="img-fluid">
-                    <form action="" enctype="multipart/form-data">
-                        {{ csrf_field() }}
-                        <input type="file">
-                        <button type="submit" class="btn btn-warning">Редактировать</button>
-                    </form>
-                    <a href="" class="btn btn-danger">Удалить</a>
-                </div>
-                <div class="item">
-                    <img src="/images/Renders/06_Evening.jpg" alt="" class="img-fluid">
-                    <form action="" enctype="multipart/form-data">
-                        {{ csrf_field() }}
-                        <input type="file">
-                        <button type="submit" class="btn btn-warning">Редактировать</button>
-                    </form>
-                    <a href="" class="btn btn-danger">Удалить</a>
-                </div>
-                <form action="" class="mt-5" enctype="multipart/form-data">
-                    {{ csrf_field() }}
-                    <label for="">Новая картинка</label>
-                    <input type="file">
-                    <button type="submit" class="btn btn-success">Еще картинка</button>
-                </form>
             </div>
         </div>
 
-        <div class="row col-12 col-md-10 justify-content-center mb-5 mx-md-auto">
-            <div class="col-12 mb-3 ml-5" ><p style="font-family: Forum; font-size: 32px; text-transform: uppercase;">
-                <form action="">
-                    <input type="text" value="Отличное расположение" class="w-50">
+        <div class="row justify-content-center mx-md-auto bg-info p-3">
+            <div class="col-12">
+                <h2 class="text-center">Секция Название</h2>
+            </div>
+            <div class="col-12 mb-3 row justify-content-center">
+                <form action="" class="col">
+                    <input type="text" value="Отличное расположение" class="col-7">
                     <button class="btn btn-warning" type="submit">Редактировать</button>
                 </form>
-                <a href="" class="btn btn-danger">Удалить</a>
-                </p><hr></div>
-            <form class="col-12 mt-4" action="">
-                <div class="col-md-1 col-1" style="margin-bottom:20px;">
-                    <img class="ml-md-3" src="/images/Icons/marker.png" style="margin-right:10px;" alt="">
-                    <input type="file">
-                </div>
-                <div class="col-md-11 col-10 mb-3">
-                    <textarea name="" id="" cols="30" rows="10">
-                        Удобное расположение комплекса позволит вам без труда добраться до работы и обратно домой
-                    </textarea>
-                </div>
-                <button class="btn btn-warning" type="submit">Редактировать</button>
-            </form>
-            <form class="col-12 mt-4" action="">
-                <div class="col-md-1 col-1" style="margin-bottom:20px;">
-                    <img class="ml-md-3" src="/images/Icons/marker.png" style="margin-right:10px;" alt="">
-                    <input type="file">
-                </div>
-                <div class="col-md-11 col-10 mb-3">
-                    <textarea name="" id="" cols="30" rows="10">
-                        Удобное расположение комплекса позволит вам без труда добраться до работы и обратно домой
-                    </textarea>
-                </div>
-                <button class="btn btn-warning" type="submit">Редактировать</button>
-            </form>
-            <form class="col-12 mt-4" action="">
-                <div class="col-md-1 col-1" style="margin-bottom:20px;">
-                    <img class="ml-md-3" src="/images/Icons/marker.png" style="margin-right:10px;" alt="">
-                    <input type="file">
-                </div>
-                <div class="col-md-11 col-10 mb-3">
-                    <textarea name="" id="" cols="30" rows="10">
-                        Удобное расположение комплекса позволит вам без труда добраться до работы и обратно домой
-                    </textarea>
-                </div>
-                <button class="btn btn-warning" type="submit">Редактировать</button>
-            </form>
-            <form class="col-12 mt-4" action="">
-                <div class="col-md-1 col-1" style="margin-bottom:20px;">
-                    <img class="ml-md-3" src="/images/Icons/marker.png" style="margin-right:10px;" alt="">
-                    <input type="file">
-                </div>
-                <div class="col-md-11 col-10 mb-3">
-                    <textarea name="" id="" cols="30" rows="10">
-                        Удобное расположение комплекса позволит вам без труда добраться до работы и обратно домой
-                    </textarea>
-                </div>
-                <button class="btn btn-warning" type="submit">Редактировать</button>
-            </form>
-            <form class="col-12 mt-4" action="">
-                <div class="col-md-1 col-1" style="margin-bottom:20px;">
-                    <img class="ml-md-3" src="/images/Icons/marker.png" style="margin-right:10px;" alt="">
-                    <input type="file">
-                </div>
-                <div class="col-md-11 col-10 mb-3">
-                    <textarea name="" id="" cols="30" rows="10">
-                        Удобное расположение комплекса позволит вам без труда добраться до работы и обратно домой
-                    </textarea>
-                </div>
-                <button class="btn btn-warning" type="submit">Редактировать</button>
-            </form>
+                <form action="" method="post" class="col-auto">
+                    @method('DELETE')
+                    <button type="submit" class="btn btn-danger">Удалить</button>
+                </form>
+            </div>
+        </div>
+        <div class="row justify-content-center mb-5 mx-md-auto bg-white">
 
+        <form class="col-12 mt-4" action="">
+                <div class="col-md-1 col-1" style="margin-bottom:20px;">
+                    <img class="ml-md-3" src="/images/Icons/marker.png" style="margin-right:10px;" alt="">
+                    <input type="file">
+                </div>
+                <div class="col-md-11 col-10 mb-3">
+                    <textarea name="" id="" cols="30" rows="10">
+                        Удобное расположение комплекса позволит вам без труда добраться до работы и обратно домой
+                    </textarea>
+                </div>
+                <button class="btn btn-warning" type="submit">Редактировать</button>
+            </form>
+            <form class="col-12 mt-4" action="">
+                <div class="col-md-1 col-1" style="margin-bottom:20px;">
+                    <img class="ml-md-3" src="/images/Icons/marker.png" style="margin-right:10px;" alt="">
+                    <input type="file">
+                </div>
+                <div class="col-md-11 col-10 mb-3">
+                    <textarea name="" id="" cols="30" rows="10">
+                        Удобное расположение комплекса позволит вам без труда добраться до работы и обратно домой
+                    </textarea>
+                </div>
+                <button class="btn btn-warning" type="submit">Редактировать</button>
+            </form>
+            <form class="col-12 mt-4" action="">
+                <div class="col-md-1 col-1" style="margin-bottom:20px;">
+                    <img class="ml-md-3" src="/images/Icons/marker.png" style="margin-right:10px;" alt="">
+                    <input type="file">
+                </div>
+                <div class="col-md-11 col-10 mb-3">
+                    <textarea name="" id="" cols="30" rows="10">
+                        Удобное расположение комплекса позволит вам без труда добраться до работы и обратно домой
+                    </textarea>
+                </div>
+                <button class="btn btn-warning" type="submit">Редактировать</button>
+            </form>
+            <form class="col-12 mt-4" action="">
+                <div class="col-md-1 col-1" style="margin-bottom:20px;">
+                    <img class="ml-md-3" src="/images/Icons/marker.png" style="margin-right:10px;" alt="">
+                    <input type="file">
+                </div>
+                <div class="col-md-11 col-10 mb-3">
+                    <textarea name="" id="" cols="30" rows="10">
+                        Удобное расположение комплекса позволит вам без труда добраться до работы и обратно домой
+                    </textarea>
+                </div>
+                <button class="btn btn-warning" type="submit">Редактировать</button>
+            </form>
+            <form class="col-12 mt-4" action="">
+                <div class="col-md-1 col-1" style="margin-bottom:20px;">
+                    <img class="ml-md-3" src="/images/Icons/marker.png" style="margin-right:10px;" alt="">
+                    <input type="file">
+                </div>
+                <div class="col-md-11 col-10 mb-3">
+                    <textarea name="" id="" cols="30" rows="10">
+                        Удобное расположение комплекса позволит вам без труда добраться до работы и обратно домой
+                    </textarea>
+                </div>
+                <button class="btn btn-warning" type="submit">Редактировать</button>
+            </form>
+        </div>
         </div>
 
         <div class="row col-12 col-md-10 justify-content-center mb-5 mx-md-auto">
