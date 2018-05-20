cbd1d731e39f9fe086fb377522609ef6f150ceaa
Uki7991 <tilek.kubanov@gmail.com>
2018-05-21 03:17:59 +0600

    BenefitController

--- app/Http/Controllers/BenefitController.php
+++ app/Http/Controllers/BenefitController.php
@@ -14,14 +14,30 @@ class BenefitController extends Controller
     }
 
     public function store(Request $request) {
-        $benefit = Benefit::create($request->all());
+        $benefit = new Benefit;
+
+        $benefit->title = $request->title;
+        if ($request->hasFile('icon')) {
+            $file = $request->file('icon');
+
+            $imageManager = new \Intervention\Image\Image();
+
+            $imageManager->make($file)
+                ->resize(100, 100, function ($constraint){
+                    $constraint->aspectRatio();
+                })
+                ->save(public_path('uploads/icons') . '/' . $file->getClientOriginalName());
+
+            $benefit->icon = $file->getClientOriginalName();
+        }
+        $benefit->menu_id = $request->menu_id;
+        $benefit->save();
 
         return redirect()->back();
     }
 
     public function update(Request $request, Benefit $benefit) {
         $benefit->title = $request->title;
-        $benefit->bcontent = $request->bcontent;
         if ($request->hasFile('icon')) {
             $file = $request->file('icon');
 
